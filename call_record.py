924c1a77778115d372f1d019bba89dbf36acda4c
Dwarakesh-V <vdwarakesh0211@gmail.com>
2026-05-21 19:25:55 +0530

    Mount path for Xiaomi_Redmi_Note_13_5G phones

--- call_record.py
+++ call_record.py
@@ -1,6 +1,39 @@
 import subprocess
 import pandas as pd
 import time
+from pathlib import Path
+import os
+
+def find_call_record_dir():
+    uid = os.getuid()
+    gvfs = Path(
+        f"/run/user/{uid}/gvfs"
+    ) # User space virtual filesystem path
+
+    if not gvfs.exists():
+        return None
+
+    for mount in gvfs.iterdir():
+
+        if (
+            mount.is_dir()
+            and mount.name.startswith(
+                "mtp:host=Xiaomi_Redmi_Note_13_5G"
+            )
+        ):
+
+            target = (
+                mount
+                / "Internal shared storage"
+                / "MIUI"
+                / "sound_recorder"
+                / "call_rec"
+            )
+
+            if target.exists():
+                return target
+
+    return None
 
 def make_call(phone):
     subprocess.run([
@@ -23,11 +56,13 @@ def end_call():
         "6"
     ]) # If the call has already ended, this does nothing
 
+print(f"Phone mounted path: {find_call_record_dir()}")
+
 numbers = list(pd.read_excel("numbers.xlsx")["phone"])
 
 for number in numbers:
     make_call(number)
-    time.sleep(60)
+    time.sleep(60) # Max 1 minute call
     end_call()
-    time.sleep(10)
-    
+    time.sleep(5) # Wait for data to sync
+
