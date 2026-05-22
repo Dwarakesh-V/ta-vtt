168edaf8eb72ed4de00dcfbe30e16e17d06d7601
Dwarakesh-V <vdwarakesh0211@gmail.com>
2026-05-22 18:11:47 +0530

    Play recorded message on start

--- call_record.py
+++ call_record.py
@@ -15,12 +15,7 @@ def find_call_record_dir():
 
     for mount in gvfs.iterdir():
 
-        if (
-            mount.is_dir()
-            and mount.name.startswith(
-                "mtp:host=Xiaomi_Redmi_Note_13_5G"
-            )
-        ):
+        if (mount.is_dir() and mount.name.startswith("mtp:host=Xiaomi_Redmi_Note_13_5G")):
 
             target = (
                 mount
@@ -28,7 +23,7 @@ def find_call_record_dir():
                 / "MIUI"
                 / "sound_recorder"
                 / "call_rec"
-            )
+            ) # Call record path
 
             if target.exists():
                 return target
@@ -46,6 +41,20 @@ def make_call(phone):
         "-d",
         f"tel:{phone}"
     ])
+
+def is_call_connecting():
+    result = subprocess.run(
+        [
+            "adb",
+            "shell",
+            "dumpsys",
+            "telecom"
+        ],
+        capture_output=True,
+        text=True
+    )
+
+    return any(["CONNECTING","DIALING"]) in result.stdout
 
 def is_call_active():
     result = subprocess.run(
@@ -70,15 +79,27 @@ def end_call():
         "6"
     ]) # If the call has already ended, this does nothing
 
+def play_audio(audio_path):
+    subprocess.run([
+        "ffplay",
+        "-nodisp",
+        "-autoexit",
+        f"{audio_path}"
+    ])
+
 print(f"Phone mounted path: {find_call_record_dir()}")
 
 numbers = list(pd.read_excel("numbers.xlsx")["phone"])
 
 for number in numbers:
     make_call(number)
+    initmsg = False
     for i in range(60):
-        if is_call_active(): # If call isnt active, break out
+        if is_call_connecting() or is_call_active(): # If call isnt active, break out
             time.sleep(1)
+            if is_call_active() and not initmsg:
+                play_audio("init_message.mp3")
+                initmsg = True
         else:
             break
     end_call()
